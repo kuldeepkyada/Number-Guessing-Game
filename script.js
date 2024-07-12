6be1387251cf4939a19673e13d91b2644adc2305
kuldeepkyada <kyada2001@gmail.com>
2024-07-12 13:34:21 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    let random = Math.ceil(Math.random()*100) + 1;
+    let guessInput = document.getElementById("guessNumber");
+    let result = document.getElementById("result");
+    let score = document.getElementById("score");
+    let guesses = 0;
+
+    document.getElementById("submitGuesses").addEventListener("click", ()=> {
+        let userGuess = parseInt(guessInput.value)
+        guesses++;
+
+        if(userGuess == random){
+            result.textContent = "Congratulations! You guess the correct number."
+            score.textContent = `Your score is: ${100 - guesses}`
+        }
+        else if (userGuess > random) {
+            result.textContent = "Your guess is too high!"
+        }
+        else{
+            result.textContent = "Your guess is too low!"
+        }
+
+        if (userGuess == random){
+            guessInput.disabled = true;
+            document.getElementById("submitGuesses").disabled = true;
+        }
+    })
+})
